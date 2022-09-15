5d9b8d37fbc61938104ac301c9707d5805b30c66
Hunter Freeman <huntercfreeman@gmail.com>
2022-09-14 20:00:03 -0400

    Video 13

---
+++ BlazorTextEditor.NativeApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using BlazorTextEditor.RazorLib;
+using Microsoft.Extensions.DependencyInjection;
+using Photino.Blazor;
+
+namespace BlazorTextEditor.NativeApp
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var appBuilder = PhotinoBlazorAppBuilder.CreateDefault(args);
+            appBuilder.Services
+                .AddLogging();
+
+            appBuilder.Services.AddBlazorTextEditorRazorLibServices();
+            
+            // register root component
+            appBuilder.RootComponents.Add<App>("app");
+
+            var app = appBuilder.Build();
+
+            // customize window
+            app.MainWindow
+                .SetIconFile("favicon.ico")
+                .SetTitle("Photino Hello World");
+
+            AppDomain.CurrentDomain.UnhandledException += (sender, error) =>
+            {
+                app.MainWindow.OpenAlertWindow("Fatal exception", error.ExceptionObject.ToString());
+            };
+
+            app.Run();
+        }
+
+    }
+}
